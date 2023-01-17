161c2886556faf21465d8ac19bfdec641ceb726a
Natsasion Srithiowat <100752017+Natsasi-on@users.noreply.github.com>
2023-01-17 06:44:47 -0500

    Update restaurant-access-data.js

--- utility/restaurant-access-data.js
+++ utility/restaurant-access-data.js
@@ -1,13 +1,11 @@
 const path = require('path');
 const fs = require('fs');
-//อ่าน ไฟล์ '..'บอกให้ย้อนไปfolder ก่อนหน้า
+
 const filePath = path.join(__dirname, '..', 'data', 'restaurantsinput.json');
 
 function getStoredRestaurants() {
-
-    //ตัวนี้จะได้ไฟล์ text มา
     const fileData = fs.readFileSync(filePath);
-    //เราเปลี่ยนเป็น array โดยเปลี่ยนเป็น json
+  
     const storedRestaurants = JSON.parse(fileData);
     return storedRestaurants;
 }
@@ -17,7 +15,6 @@ function storeRestaurants(storableRestaurants) {
 }
 
 module.exports = {
-    // อะไรก็ได้ : name of function
     mygetStoredRestaurants: getStoredRestaurants,
     mystoredRestaurants: storeRestaurants,
 }
