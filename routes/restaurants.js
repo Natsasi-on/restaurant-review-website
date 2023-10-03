f5583c509ff80d74f46e2d3a3f6618e7d7a69cc4
Natsasion Srithiowat <100752017+Natsasi-on@users.noreply.github.com>
2023-10-03 02:14:31 -0400

    Update restaurants.js

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,77 +1,103 @@
 const express = require('express');
-const uuid = require('uuid');
-
-const resData = require('../utility/restaurant-access-data');
 const router = express.Router();
+const uuid = require('uuid');
+const restaurantData = require('../utility/restaurant-access-data');
 
+// Middleware to parse URL-encoded data
+router.use(express.urlencoded({ extended: false }));
 
+// GET route for recommending a restaurant
+router.get('/recommend', function (req, res) {
+    res.render('recommend', { mytitle: "Recommend Restaurant" });
+});
 
+// POST route for submitting a restaurant recommendation
+router.post('/recommend', function (req, res) {
+    // Get the submitted restaurant data from the request body
+    const newRestaurant = req.body;
+
+    // Generate a unique ID for the restaurant
+    newRestaurant.id = uuid.v4();
+
+    // Retrieve the currently stored restaurants
+    const storedRestaurants = restaurantData.getStoredRestaurants();
+
+    // Add the new restaurant to the list of stored restaurants
+    storedRestaurants.push(newRestaurant);
+
+    // Update the stored restaurant data
+    restaurantData.storeRestaurantData(storedRestaurants);
+
+    // Redirect to the confirmation page
+    res.redirect('/confirm');
+});
+
+// GET route for displaying the confirmation page
+router.get('/confirm', function (req, res) {
+    res.render('confirm', { mytitle: "Confirm" });
+});
+
+// GET route for displaying the list of reviewed restaurants
 router.get('/restaurants', function (req, res) {
+    // Get the 'order' query parameter from the URL
     let order = req.query.order;
     let nextOrder = 'desc';
+
+    // Validate the 'order' parameter
     if (order !== 'asc' && order !== 'desc') {
         order = 'asc';
     }
+
+    // Determine the 'nextOrder' value for toggling order
     if (order === 'desc') {
         nextOrder = 'asc';
     }
-    const storedRestaurants = resData.mygetStoredRestaurants();
 
-    storedRestaurants.sort(function (restaurantA, restaurantB) {
-        if ((order === 'asc' && restaurantA.name > restaurantB.name) || (order === 'desc' && restaurantB.name > restaurantA.name)) {
-            return 1 
+    // Retrieve the stored restaurant data
+    const storedRestaurants = restaurantData.getStoredRestaurants();
+
+    // Sort the restaurants based on the selected order
+    storedRestaurants.sort(function (resA, resB) {
+        if (
+            (order === 'asc' && resA.name.toUpperCase() > resB.name.toUpperCase()) ||
+            (order === 'desc' && resB.name.toUpperCase() > resA.name.toUpperCase())
+        ) {
+            return 1;
         }
-        return -1 
+        return -1;
     });
 
+    // Render the restaurants page with data
     res.render('restaurants', {
+        mytitle: "Reviewed Restaurants",
         numberOfRestaurants: storedRestaurants.length,
-        restaurants: storedRestaurants,
+        storedRestaurants: storedRestaurants,
         nextOrder: nextOrder,
     });
 });
 
+// GET route for displaying details of a specific restaurant by ID
 router.get('/restaurants/:id', function (req, res) {
+    // Get the restaurant ID from the URL parameters
     const restaurantId = req.params.id;
 
-    const storedRestaurants = resData.mygetStoredRestaurants();
-    
-    for (const rest of storedRestaurants) {
-        if (rest.id === restaurantId) {
+    // Retrieve the stored restaurant data
+    const storedRestaurants = restaurantData.getStoredRestaurants();
+
+    // Find the restaurant with the matching ID
+    for (const restaurant of storedRestaurants) {
+        if (restaurant.id === restaurantId) {
+            // Render the restaurant detail page with the found restaurant data
             return res.render('restaurant-detail', {
-                restaurant: rest,
+                mytitle: "Restaurant Detail",
+                restaurant: restaurant,
             });
         }
     }
-    res.render('404');
+
+    // If no matching restaurant is found, render a 404 page
+    res.render('404', { mytitle: "Page not found" });
 });
 
-router.get('/recommend', function (req, res) {
-    res.render('recommend');
-});
-
-router.post('/recommend', function (req, res) {
-   
-    const restaurant = req.body;
-    
-    restaurant.id = uuid.v4();
-  
-    const storedRestaurants = resData.mygetStoredRestaurants();
-
-    storedRestaurants.push(restaurant);
-    
-    resData.mystoredRestaurants(storedRestaurants);
- 
-    res.redirect('/confirm');
-});
-
-router.get('/confirm', function (req, res) {
-    res.render('confirm');
-});
-
-router.get('/login', function (req, res) {
-    res.render('login');
-});
-
-
+// Export the router for use in other parts of the application
 module.exports = router;
