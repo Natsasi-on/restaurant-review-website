46543419d44f59c4a9a49e045a733047248a519d
Natsasion Srithiowat <100752017+Natsasi-on@users.noreply.github.com>
2023-10-03 02:15:47 -0400

    Update restaurant-access-data.js

--- utility/restaurant-access-data.js
+++ utility/restaurant-access-data.js
@@ -1,20 +1,33 @@
+// Import necessary modules
 const path = require('path');
 const fs = require('fs');
 
+// Define the file path for the JSON data
 const filePath = path.join(__dirname, '..', 'data', 'restaurantsinput.json');
 
+// Function to retrieve stored restaurant data from the JSON file
 function getStoredRestaurants() {
+    // Read the content of the JSON file
     const fileData = fs.readFileSync(filePath);
-  
+
+    // Parse the JSON data into an array of restaurants
     const storedRestaurants = JSON.parse(fileData);
+
+    // Return the array of stored restaurants
     return storedRestaurants;
 }
 
-function storeRestaurants(storableRestaurants) {
-    fs.writeFileSync(filePath, JSON.stringify(storableRestaurants));
+// Function to store restaurant data in the JSON file
+function storeRestaurantData(storableRestaurants) {
+    // Serialize the array of restaurants into JSON format
+    const jsonData = JSON.stringify(storableRestaurants);
+
+    // Write the JSON data back to the file, overwriting the existing content
+    fs.writeFileSync(filePath, jsonData);
 }
 
+// Export the functions for use in other parts of the application
 module.exports = {
-    mygetStoredRestaurants: getStoredRestaurants,
-    mystoredRestaurants: storeRestaurants,
+    getStoredRestaurants: getStoredRestaurants,
+    storeRestaurantData: storeRestaurantData
 }
